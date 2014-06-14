89d8bda83cccbd9d69d18ea20c85faab3b414d5a
yunik <skadvs@gmail.com>
2014-06-15 01:18:22 +0900

    Implement Packet Caputer

---
+++ History Search Engine/Client/Http Parser/HttpPacketCapture.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SharpPcap;
+using PacketDotNet;
+
+namespace Client.Http_Parser
+{
+    class HttpPacketCapture
+    {
+        private ICaptureDevice device;
+
+        /**
+         * Default Constructor
+         **/
+        public HttpPacketCapture()
+        {
+            device = null;
+        }
+
+        public CaptureDeviceList getEthernetDeviceList()
+        {
+            var devices = CaptureDeviceList.Instance;
+
+            if (devices.Count < 1)
+            {
+                return null;
+            }
+            return devices;
+        }
+
+        public void setDevice(ICaptureDevice device)
+        {
+            this.device = device;
+        }
+
+        public void start(int timeout)
+        {
+            device.OnPacketArrival += new SharpPcap.PacketArrivalEventHandler(onPacketArrival);
+            device.Open(DeviceMode.Promiscuous, timeout);
+            device.StartCapture();
+        }
+
+        public void stop()
+        {
+            device.StopCapture();
+        }
+
+        private void onPacketArrival(object sender, CaptureEventArgs e)
+        {
+            DateTime time = e.Packet.Timeval.Date;
+            int len = e.Packet.Data.Length;
+            Console.WriteLine("{0}:{1}:{2}:{3} Len={4}", time.Hour, time.Minute, time.Second, time.Millisecond, len);
+            Console.WriteLine("{0}", e.Packet.Data);
+        }
+    }
+}
--- History Search Engine/Client/Program.cs
+++ History Search Engine/Client/Program.cs
@@ -4,12 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Client.Http_Parser;
+using SharpPcap;
+
+
 namespace Client
 {
     class Program
     {
         static void Main(string[] args)
         {
+            System.Diagnostics.Debug.WriteLine("HSE Client Debug Console");
+
+            HttpPacketCapture capture = new HttpPacketCapture();
+            CaptureDeviceList devices = capture.getEthernetDeviceList();
+            ICaptureDevice device = devices[2];
+
+            foreach(ICaptureDevice element in devices)
+            {
+                Console.WriteLine("{0}", element.ToString());
+            }
+
+            capture.setDevice(device);
+
+            Console.WriteLine("--Listening on {0}, hit 'Enter' to stop...", device.Name);
+
+            capture.start(1000);
+
+            Console.ReadLine();
+
+            capture.stop();
         }
     }
 }
