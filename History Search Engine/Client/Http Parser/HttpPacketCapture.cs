0f8585b3a2f7c896175ebbaf19ab2da8a33e9453
yunik <skadvs@gmail.com>
2014-06-25 21:45:06 +0900

    Fix Filtering Condition

--- History Search Engine/Client/Http Parser/HttpPacketCapture.cs
+++ History Search Engine/Client/Http Parser/HttpPacketCapture.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using SharpPcap;
 using PacketDotNet;
 
@@ -72,20 +71,25 @@ namespace Client.Http_Parser
         {
             DateTime time = e.Packet.Timeval.Date;
             int len = e.Packet.Data.Length;
+
             var rawSourcePort = new Byte[2];
-            Array.Copy(e.Packet.Data, 15, rawSourcePort, 0, 2);
+            Array.Copy(e.Packet.Data, 34, rawSourcePort, 0, 2);
             string sourcePort = ByteArrayToString(rawSourcePort);
-            System.Diagnostics.Debug.WriteLine("{0}:{1}:{2} Source Port={3}", time.Hour, time.Minute, time.Second, sourcePort);
-            if (sourcePort == "0005")
+
+            var rawDesinationPort = new Byte[2];
+            Array.Copy(e.Packet.Data, 36, rawDesinationPort, 0, 2);
+            string desinationPort = ByteArrayToString(rawDesinationPort);
+
+            if (String.Compare(sourcePort,"0050")==0 || String.Compare(desinationPort, "0050")==0)
             {
                 Console.WriteLine("{0}:{1}:{2}:{3} Len={4}", time.Hour, time.Minute, time.Second, time.Millisecond, len);
-                Console.WriteLine("{0}", sourcePort);
+                Console.WriteLine("{0}", ByteArrayToString(e.Packet.Data));
             }
         }
 
         private static string ByteArrayToString(byte[] array)
         {
-            string hex = BitConverter. ToString(array);
+            string hex = BitConverter.ToString(array);
             return hex.Replace("-", "");
         }
     }
--- History Search Engine/Client/Program.cs
+++ History Search Engine/Client/Program.cs
@@ -6,16 +6,19 @@ using System.Threading.Tasks;
 using Client.Service.Network;
 using log4net;
 using Client.Utility;
+using Client.Http_Parser;
 
 namespace Client
 {
     public class Program
     {
         private static ILog logger = LogManager.GetLogger(typeof(Program));
+        private static HttpPacketCapture packetCapture = new HttpPacketCapture();
 
         public static void Main(string[] args)
         {
-            StartNetworkingService();
+            packetCapture.Start(1000);
+            //StartNetworkingService();
         }
 
         private static void StartNetworkingService()
