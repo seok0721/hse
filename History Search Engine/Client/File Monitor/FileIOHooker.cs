de5e01b872070ede0a687a62b7ae4b3b45a317d6
yunik <skadvs@gmail.com>
2014-07-01 08:28:29 +0900

    Add File I/O monitoring class

---
+++ History Search Engine/Client/File Monitor/FileIOHooker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.File_Monitor
+{
+    class FileIOHooker
+    {
+        /// <summary>
+        /// Basic constructor
+        /// </summary>
+        public FileIOHooker()
+        {
+
+        }
+
+    }
+}
